94a7f22dd2ed5756c34d7b77ed841dd2a11cb2f2
Christian Ramos <christianramosarias2@gmail.com>
2025-05-13 15:29:09 -0500

    feat se le ponen estilos al proyecto

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -3,12 +3,12 @@ import MenuLateral from "../components/MenuLateral"
 import Contenido from "../components/Contenido"
 const Servicios = () => {
     return (
-        <div className="servcios">
+        <div className="servcio">
             <MenuLateral />
-            <div className="servicios-contenido">
-                <main className="contacto-principal">
-                    <section className="contacto-titulo">
-                        <h2 className="contacto-titulo-texto">Servicios</h2>
+            <div className="servicio-contenido">
+                <main className="servicio-principal">
+                    <section className="servicio-titulo">
+                        <h2 className="servicio-titulo-texto">Servicios</h2>
                     </section>
                     <Contenido />
                 </main>
